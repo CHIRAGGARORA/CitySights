38173a5f99a1d3884c54f5d149d5d5c621aa0a15
Chirag Arora <arora2002chirag@gmail.com>
2024-10-23 16:36:05 +0530

    - add dataservice and business search method api call - fire off api call when main view appears

--- CitySights/ContentView.swift
+++ CitySights/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var query: String = ""
+    var service = DataService()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            TextField("What are you looking for?", text: $query)
+            Button {
+                // TODO
+            } label: {
+                Text("Go")
+                    .padding(.horizontal)
+                    .padding(.vertical, 10)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+            }
+
         }
         .padding()
+        .task {
+            await service.businessSearch()
+        }
     }
 }
 
