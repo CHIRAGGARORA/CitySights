f8250a7da4fc66234de2d095dff96ca817a288e3
Chirag Arora <arora2002chirag@gmail.com>
2024-10-23 12:58:05 +0530

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by chirag arora on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySights
+//
+//  Created by chirag arora on 23/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
