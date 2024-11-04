4cbea32786605ea28cc2d397ad3a3fa1283a8a17
Chirag Arora <arora2002chirag@gmail.com>
2024-11-04 16:55:09 +0530

    - enable images dunamic in business List view - enable dynamic image in business detail view

--- CitySights/ListView.swift
+++ CitySights/ListView.swift
@@ -18,8 +18,29 @@ struct ListView: View {
             ForEach(businessModel.businesses) { business in
                 VStack (spacing: 20) {
                     HStack (spacing: 0) {
-                        Image("list-placeholder-image")
-                            .padding(.trailing, 16)
+                        
+                        if let imageUrl = business.imageUrl {
+                            // Display the business image
+                            AsyncImage(url: URL(string: imageUrl)!) { image in
+                                // here image is the returned imageComponent which is downloaded
+                                image
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipped()
+                                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                                    .padding(.trailing, 16)
+                            } placeholder: {
+                                ProgressView()
+                                    .frame(width: 50, height: 50)
+                            }
+
+                        }
+                        else {
+                            Image("list-placeholder-image")
+                                .padding(.trailing, 16)
+                        }
+                        
                         VStack (alignment: .leading) {
                             Text(business.name ?? "Restaurant")
                                 .font(Font.system(size: 15))
